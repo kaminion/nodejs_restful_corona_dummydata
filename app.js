b94ae4420f095c4bb4900efd842d12597538c19c
Declan Andrew <anysweetimes@naver.com>
2020-03-10 03:11:16 +0900

    [2020-03-10 03:11] serverTest

--- app.js
+++ app.js
@@ -10,21 +10,20 @@ function randomFunc(min, max)
 app.get(["/", "/storesByGeo/json"], (req, res) =>
 {
     let stores = [];
-
-    for(let i=0;i<50;i++)
+    let stat = ['plenty', 'some', 'few', 'empty'];
+    for(let i=0;i<50000;i++)
     {
         let store = 
         {
-            "code": "string",
-            "name": "테스트 병원",
+            "code": "식별코드",
+            "name": "테스트 약국",
             "addr": "테스트 주소",
-            "lat": randomFunc(33, 43),
-            "lng": randomFunc(132, 124),
-            "stock_t": "2시",
-            "stock_cnt": Math.floor(randomFunc(1, 10)),
+            "type": "약국",
+            "lat": randomFunc(37, 37), // randomFunc(33, 43)
+            "lng": randomFunc(127, 127), // randomFunc(132, 124)
+            "stock_at": "입고시간",
             "sold_cnt": Math.floor(randomFunc(1, 10)),
-            "remain_cnt": Math.floor(randomFunc(1, 10)),
-            "sold_out": true || false,
+            "remain_stat": stat[Math.floor(randomFunc(0, 4))], // 0~3까지
             "created_at": Date()
         };
         stores.push(store);
